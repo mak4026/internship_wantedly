edc0951314d5dbd418510104c6a8b99351cfb97a
mak4026 <chikuwa2135@gmail.com>
2016-06-27 19:54:52 +0900

    テスト周り追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  def setup
+    @user = users( :john )
+    log_in(@user)
+  end
+
   test "should get home" do
     get :home
     assert_response :success
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,26 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
 
-  test "layout links" do
+  def setup
+    Warden.test_mode!
+    @user = users( :john )
+  end
+
+  test "layout links not signed in" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", new_user_registration_path
+    assert_select "a[href=?]", new_user_session_path
+  end
+
+  test "layout links when signed in" do
+  	log_in(@user)
+  	get root_path
+  	assert_template 'static_pages/home'
+  	assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", destroy_user_session_path
   end
 end
