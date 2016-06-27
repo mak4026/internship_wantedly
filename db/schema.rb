aa86284671afc9371a369bcbee7368f3968a15c3
mak4026 <chikuwa2135@gmail.com>
2016-06-28 00:14:04 +0900

    skillsモデル追加、テスト追加、fixture追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625180758) do
+ActiveRecord::Schema.define(version: 20160627140853) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "skills", ["name"], name: "index_skills_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
