b8a0c19ceb1c9ed4a322cd141a17788312547fe3
mak4026 <chikuwa2135@gmail.com>
2016-06-28 23:00:15 +0900

    schema.rb修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20160627172924) do
 
+  create_table "skills", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "skills", ["name"], name: "index_skills_on_name", unique: true
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
