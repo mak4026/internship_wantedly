73627bcf532f9306bc955a5eff26047a35ca533d
mak4026 <chikuwa2135@gmail.com>
2016-07-02 03:31:09 +0900

    test追加、それに伴うcontroller修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user,   only: :destroy
   def create
   	user = User.find(params[:user_id])
   	ability = Skill.find(params[:skill_id])
@@ -12,10 +13,15 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    relation = current_user.add_skill.find(params[:id])
-    skill = Skill.find(relation.skill_id)
-    relation.destroy
+    skill = Skill.find(@relation.skill_id)
+    @relation.destroy
     flash[:notice] = skill.name+" を -1 しました"
-    redirect_to user_path id: relation.user_id
+    redirect_to user_path id: @relation.user_id
+  end
+
+private
+  def correct_user
+    @relation = current_user.add_skill.find_by(id: params[:id])
+    redirect_to root_path if @relation.nil?
   end
 end
---
+++ test/integration/skill_plus_minus_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class SkillPlusMinusTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+  def setup
+    Warden.test_mode!
+    @user = users(:archer)
+    @other = users(:john)
+    @ruby = skills(:ruby)
+    @java = skills(:java)
+  end
+
+  test "skill plus and minus" do
+    log_in(@user)
+    get user_path @other
+    assert_match @ruby.name, response.body
+    assert_match @java.name, response.body
+    assert_difference 'Relationship.count', 1 do
+      post relationships_path, {user_id: @other.id, skill_id: @ruby.id}
+    end
+    assert_redirected_to @other
+    follow_redirect!
+    assert_match @ruby.name, response.body
+    assert_match @java.name, response.body
+    assert_difference 'Relationship.count', -1 do
+      delete relationship_path relationships(:three)
+    end
+  end
+
+  test "can't another adder skill -1" do
+    log_in(@user)
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path relationships(:zero)
+    end
+  end
+
+  test "can't +1 already +1ed skill" do
+    log_in(@user)
+    assert_no_difference 'Relationship.count' do
+     post relationships_path, {user_id: @other.id, skill_id: @java.id}
+    end
+  end
+end
