7aac7d8c699255683427f1dbf31933e0df6ea2f2
mak4026 <chikuwa2135@gmail.com>
2016-07-02 05:48:22 +0900

    deviseのstrong parameterの書き方を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) {|u|
-        u.permit(:name, :email, :password, :password_confirmation)
-      }
-      devise_parameter_sanitizer.for(:sign_in) {|u|
-        u.permit(:email, :password)
-      }
-      devise_parameter_sanitizer.for(:account_update) {|u|
-        u.permit(:name, :email, :password, :password_confirmation, :current_password)
-     }
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
 end
